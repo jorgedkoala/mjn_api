ed6ed01241e4c2eccc422a6c5967ca229340b763
jorge dominguez <jorged@MacBook-Pro-de-jorge.local>
2017-11-15 17:40:53 +0100

    start

---
+++ inc/conexion.php
@@ -0,0 +1,6 @@
+<?php
+//$conexion = mysqli_connect('localhost', 'mjn_user', 'cRfi04?4', 'MJN')or
+$conexion = mysqli_connect('myband-1324:europe-west1:koala01', 'tfc', 'yzf600r6$#', 'MJN')or
+die("Problemas al conectar" . mysqli_error());
+mysqli_set_charset($conexion,'utf8');
+?>
---
+++ login.php
@@ -0,0 +1,44 @@
+<?php 
+header('Access-Control-Allow-Origin: *');  
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
+include("./inc/conexion.php");
+require_once('./inc/jwt.php');
+use \Firebase\JWT\JWT;
+$key = "mjnKeyForToken";
+$user = $_GET["user"];
+$password = $_GET["password"];
+//$token = strrev( str_replace(".","",$_SERVER['SERVER_ADDR']) );
+$sql = "select * from users where user = '" . $user . "' and password = '" . $password . "'";
+$registros=mysqli_query($conexion,$sql) or die('{"success":"false","error":"query->'.mysqli_error($conexion).$sql.'"}');
+//echo $sql;
+$numero_filas = 0;
+while ($reg=mysqli_fetch_array($registros))
+{	
+$numero_filas ++;
+$rows[] = $reg;
+$role = $reg["id"];
+$user = $reg["tipouser"];
+}
+
+if($numero_filas){
+$issuedat= time();
+$expire = $issuedat + 999936000;
+$token = array(
+    "iss" => "http://mjn.cat", //IDENTIFICADOR DE DOMINIO
+    "aud" => "http://mjn.cat", //
+    "iat" => $issuedat,//1356999524, // Issued at: time when the token was generated
+  //  "nbf" => $notbefore,
+    "exp" => $expire,
+    "rol"=> $role, // Not before
+    "jti"=> $user
+);
+$jwt = JWT::encode($token, $key);
+
+$result = '{"success":"true","token":"' .$jwt . '" ,"data":' .json_encode($rows) .'}';
+}
+else {
+$result = '{"success":"false","error":"Usuario o password no válidos"}';
+}
+print json_encode($result);
+
+?>
